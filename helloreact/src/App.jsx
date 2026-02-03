86945d00a433c830dfa858f0f7c37c18724d3bca
rafaellindemann <rafaelld@gmail.com>
2026-02-02 22:04:21 -0300

    att helloreact

--- helloreact/src/App.jsx
+++ helloreact/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Cabecalho from './components/Cabecalho';
 
 function App() {
 
 
   return (
     <>
+      <Cabecalho />
       <h1>Hello React</h1>
+      <p>Parágrafo no App</p>
     </>
   )
 }
---
+++ helloreact/src/components/Cabecalho.jsx
@@ -0,0 +1,15 @@
+import './Cabecalho.css'
+function Cabecalho() {
+
+    return (
+        <header>
+            Cabeçalho da página
+            <p class='paragrafo-do-header'>Textinho qualquer</p>
+        </header>
+    )
+}
+
+export default Cabecalho;
+
+
+
